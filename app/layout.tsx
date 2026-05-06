654969c11752e1e78a38447d7e2df74d196e569b
Eryk Kunasz <eryk.kunasz@gmail.com>
2026-05-06 15:26:26 +0200

    chore: pnpm lock + dark mode + Roboto font
    - dodaj pnpm-lock.yaml i pin packageManager@10.33.2
    - dark class na <html>, czcionka Roboto zamiast Geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const roboto = Roboto({
+  variable: "--font-sans",
+  subsets: ["latin", "latin-ext"],
+  weight: ["300", "400", "500", "700"],
 });
 
-const geistMono = Geist_Mono({
+const robotoMono = Roboto_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  weight: ["400", "500"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="pl"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${roboto.variable} ${robotoMono.variable} dark h-full antialiased`}
     >
       <body className="h-full flex flex-col">{children}</body>
     </html>
